fa30e56d2f55954568ca6fcccb774f7ba260bf96
Mu-An Chiou <me@muanchiou.com>
2014-10-29 13:17:57 +0800

    consider number of followers

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ var req = require('request')
 var cheerio = require('cheerio')
 var username = process.argv[2]
 var commitsNeeded = 0
-var commitsHad = 0
+var commitsHas= 0
+var followersNeeded = 0
+var followersHas = 0
+var headers = {'user-agent': 'https://github.com/muan/gntu'}
 
 if(!username) {
   console.log('💥  give me a username to check.')
@@ -23,21 +26,29 @@ function start() {
     }
     if(response.statusCode === 200) {
       $ = cheerio.load(data)
-      commitsHad = $('.contrib-number').text().split(' ')[0]
-      process.stdout.write(' ' + commitsHad + '!\n')
+      commitsHas = $('.contrib-number').text().split(' ')[0]
+      process.stdout.write(' ' + commitsHas + '!\n')
 
-      findLowest()
+      req({url: 'https://api.github.com/users/' + username, headers: headers }, function (err, response, data) {
+        if(err) console.log(err)
+        if(response.statusCode === 200) {
+          data = JSON.parse(data)
+          followersHas = data.followers
+          findLowest()
+        }
+      })
     }
   })
 }
 
 function findLowest() {
   process.stdout.write('finding out the lowest number of commits @' + username + ' will need...')
-  req({ url: 'https://api.github.com/gists/2657075', headers: {'user-agent': 'https://github.com/muan/gntu'} }, function (err, response, data) {
+  req({ url: 'https://api.github.com/gists/2657075', headers: headers }, function (err, response, data) {
     if(err) console.log(err)
     if(response.statusCode === 200) {
       data = JSON.parse(data)
       var content = data.files['active.md'].content
+      followersNeeded = content.match(/user.followers > ([0-9]+)\)/)[1]
       var table = content.split('<table cellspacing="0">')[1].split('</table>')[0]
       $ = cheerio.load(table)
 
@@ -50,9 +61,16 @@ function findLowest() {
 }
 
 function grandReveal() {
-  var diff = commitsNeeded - commitsHad
+  var diff = commitsNeeded - commitsHas
+  var fdiff = followersNeeded - followersHas
   if(diff > 0) {
-    console.log('@' + username + ' needs ' + diff + ' more commits to be on *the* list.')
+    if(fdiff > 0) {
+      console.log('@' + username + ' needs ' + diff + ' more commits and ' + fdiff + ' more followers to be on *the* list.')
+    } else {
+      console.log('@' + username + ' needs ' + diff + ' more commits to be on *the* list.')
+    }
+  } else if(followersNeeded > followersHas) {
+    console.log('@' + username + ' should be on that list BUT does not have meet the followers requirement :(')
   } else {
     console.log('@' + username + ' should be on that list already!')
   }
